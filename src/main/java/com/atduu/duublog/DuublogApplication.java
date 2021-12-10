516d88e4488db4f415aecf313d13dbabcd22fadf
一入夫人 <2895241570@qq.com>
2021-12-11 00:31:20 +0800

    第四天：开始后端

---
+++ src/main/java/com/atduu/duublog/DuublogApplication.java
@@ -0,0 +1,13 @@
+package com.atduu.duublog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuublogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DuublogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/atduu/duublog/DuublogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atduu.duublog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuublogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
